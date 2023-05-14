39127a9865df0ae32786ccd470e4cfc4b24960aa
slayzerg01 <bagdat250716@gmail.com>
2023-05-14 18:31:18 +0500

    initial

---
+++ main.py
@@ -0,0 +1,349 @@
+import sys
+
+from PyQt5 import QtWidgets
+from PyQt5.QtCore import Qt
+from PyQt5.QtWidgets import QApplication
+from PyQt5 import uic
+
+from OpenGL.GL import *
+from OpenGL.GLU import *
+
+SPHERE_RADIUS = 0.7
+CYLINDER_RADIUS = 0.2
+
+
+class MainForm(QtWidgets.QMainWindow):
+    def __init__(self, *args):
+        super(MainForm, self).__init__(*args)
+        self.resetScene()
+        self.lastPos = None
+        uic.loadUi("ui/MainForm.ui", self)
+        self.setupUi()
+
+    def setupUi(self):
+        self.windowHeight = self.openGLWidget.height()
+        self.windowWidth = self.openGLWidget.width()
+        self.openGLWidget.initializeGL()
+        self.openGLWidget.resizeGL(self.windowWidth, self.windowHeight)
+        self.openGLWidget.paintGL = self.paintGL
+        self.openGLWidget.initializeGL = self.initializeGL
+
+        self.pB_xUp.clicked.connect(self.xUp)
+        self.pB_xDown.clicked.connect(self.xDown)
+        self.pB_yUp.clicked.connect(self.yUp)
+        self.pB_yDown.clicked.connect(self.yDown)
+        self.pB_zUp.clicked.connect(self.zUp)
+        self.pB_zDown.clicked.connect(self.zDown)
+        self.pB_reset.clicked.connect(self.resetScene)
+        self.pB_zoomUp.clicked.connect(self.zoomUp)
+        self.pB_zoomDown.clicked.connect(self.zoomDown)
+        self.pB_perUp.clicked.connect(self.perUp)
+        self.pB_perDown.clicked.connect(self.perDown)
+
+    def resizeEvent(self, event):
+        size = event.size()
+        self.openGLWidget.resize(size.width() - 156, size.height() - 21)
+
+    def mousePressEvent(self, event):
+        if event.button() == Qt.LeftButton:
+            self.lastPos = event.pos()
+
+    def mouseMoveEvent(self, event):
+        if event.buttons() & Qt.LeftButton:
+            dx = event.x() - self.lastPos.x()
+            dy = event.y() - self.lastPos.y()
+            self.xRot += dy
+            self.yRot += dx
+            self.lastPos = event.pos()
+            self.update()
+
+    def wheelEvent(self, event):
+        if event.angleDelta().y() > 0:
+            self.perDown()
+        else:
+            self.perUp()
+        self.update()
+
+    def initializeGL(self):
+        glClearColor(0.5, 0.5, 0.5, 1.0)
+        glShadeModel(GL_SMOOTH)
+        glEnable(GL_CULL_FACE)
+        glEnable(GL_DEPTH_TEST)
+        glEnable(GL_LIGHTING)
+
+        lightPosition = [10.0, -30.0, 10.0, 1.0]
+        lightColor = [0.9, 0.9, 0.9, 1.0]
+        glLightfv(GL_LIGHT0, GL_POSITION, lightPosition)
+        glLightfv(GL_LIGHT0, GL_DIFFUSE, lightColor)
+        glEnable(GL_LIGHT0)
+
+    def loadScene(self):
+        glMatrixMode(GL_PROJECTION)
+        glLoadIdentity()
+        gluPerspective(
+            self.perspective,
+            self.windowWidth / float(self.windowHeight or 1),
+            0.1,
+            100
+        )
+
+        glMatrixMode(GL_MODELVIEW)
+        glLoadIdentity()
+        gluLookAt(
+            10, -20, 15,
+            0, 0, 1,
+            0, 0, 1
+        )
+
+    def paintGL(self):
+        self.loadScene()
+        glClear(GL_COLOR_BUFFER_BIT | GL_DEPTH_BUFFER_BIT)
+
+        glPushMatrix()
+
+        glRotate(self.xRot, 1.0, 0.0, 0.0)
+        glRotate(self.yRot, 0.0, 1.0, 0.0)
+        glRotate(self.zRot, 0.0, 0.0, 1.0)
+        glScalef(self.zoom, self.zoom, self.zoom)
+        # glTranslatef(0.0, 0.0, self.zoom)
+
+        glPushMatrix()
+        glTranslate(-3.0, 3.0, -3.0)
+        glRotate(90, 1.0, 0.0, 0.0)
+        self.ferum()
+        glPopMatrix()
+
+        glPopMatrix()
+
+        self.openGLWidget.update()
+
+    def ferum(self):
+
+        # Рисуем сферы
+        color = [1.0, 1.0, 0.0, 1.0]
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color)
+        object = gluNewQuadric()
+
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+
+        glPushMatrix()
+        glTranslate(0.0, 0.0, 6.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 0.0, 6.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 0.0, 6.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 0.0, 0.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 6.0, 0.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 6.0, 6.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 6.0, 6.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 6.0, 0.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(3.0, 3.0, 3.0)
+        gluSphere(object, SPHERE_RADIUS, 100, 100)
+        glPopMatrix()
+
+        # Рисуем связи между сферами
+        color = [1.0, 1.0, 1.0, 1.0]
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color)
+
+        glPushMatrix()
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 0.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 6.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 6.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glRotate(-90, 1.0, 0.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 0.0, 0.0)
+        glRotate(-90, 1.0, 0.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 0.0, 6.0)
+        glRotate(-90, 1.0, 0.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 0.0, 6.0)
+        glRotate(-90, 1.0, 0.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glRotate(90, 0.0, 1.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 6.0, 0.0)
+        glRotate(90, 0.0, 1.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 6.0, 6.0)
+        glRotate(90, 0.0, 1.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 0.0, 6.0)
+        glRotate(90, 0.0, 1.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 6, 100, 100)
+        glPopMatrix()
+
+        # Диагональные линии
+        glPushMatrix()
+        glRotate(45, 0.0, 1.0, 0.0)
+        glRotate(-35, 1.0, 0.0, 0.0)
+        glRotate(45, 0.0, 0.0, 1.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 10, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 0.0, 0.0)
+        glRotate(-45, 0.0, 1.0, 0.0)
+        glRotate(-35, 1.0, 0.0, 0.0)
+        glRotate(-45, 0.0, 0.0, 1.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 10, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(0.0, 0.0, 6.0)
+        glRotate(135, 0.0, 1.0, 0.0)
+        glRotate(-35, 1.0, 0.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 10, 100, 100)
+        glPopMatrix()
+
+        glPushMatrix()
+        glTranslate(6.0, 0.0, 6.0)
+        glRotate(-135, 0.0, 1.0, 0.0)
+        glRotate(-35, 1.0, 0.0, 0.0)
+        gluCylinder(object, CYLINDER_RADIUS, CYLINDER_RADIUS, 10, 100, 100)
+        glPopMatrix()
+
+    def coordinates(self):
+        color = [1.0, 0.0, 0.0, 1.0]
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color)
+        glBegin(GL_LINES)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(10.0, 0.0, 0.0)
+        glEnd()
+
+        color = [0.0, 1.0, 0.0, 1.0]
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color)
+        glBegin(GL_LINES)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 10.0, 0.0)
+        glEnd()
+
+        color = [0.0, 0.0, 1.0, 1.0]
+        glMaterialfv(GL_FRONT_AND_BACK, GL_DIFFUSE, color)
+        glBegin(GL_LINES)
+        glVertex3f(0.0, 0.0, 0.0)
+        glVertex3f(0.0, 0.0, 10.0)
+        glEnd()
+
+    def resetScene(self):
+        self.perspective = 30
+        self.xRot = 0
+        self.yRot = 0
+        self.zRot = 0
+        self.zoom = 1
+
+    def xUp(self):
+        self.xRot += 10
+        self.paintGL()
+
+    def xDown(self):
+        self.xRot -= 10
+        self.paintGL()
+
+    def yUp(self):
+        self.yRot += 10
+        self.paintGL()
+
+    def yDown(self):
+        self.yRot -= 10
+        self.paintGL()
+
+    def zUp(self):
+        self.zRot += 10
+        self.paintGL()
+
+    def zDown(self):
+        self.zRot -= 10
+        self.paintGL()
+
+    def zoomUp(self):
+        self.zoom += 0.1
+
+    def zoomDown(self):
+        self.zoom -= 0.1
+
+    def perUp(self):
+        if self.perspective < 90:
+            self.perspective += 5
+
+    def perDown(self):
+        if self.perspective > 5:
+            self.perspective -= 5
+
+
+def main():
+    app = QApplication(sys.argv)
+    window = MainForm()
+    window.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == '__main__':
+    main()
